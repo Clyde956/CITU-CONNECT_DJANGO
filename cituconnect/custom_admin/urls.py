d8b505e7052abdaea93bb580b6be8e105366f3de
Clyde-Bacars <john.bacarisas@cit.edu>
2024-12-08 20:07:33 +0800

    Approve Reject Posts
    Approve and Rejection of posts are now working on the admin side.

--- cituconnect/custom_admin/urls.py
+++ cituconnect/custom_admin/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import dashboard, update_post_status
 from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='custom_admin_dashboard'),
+    path('update_post_status/<int:post_id>/', update_post_status, name='update_post_status'),
 ]
